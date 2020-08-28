24059c0c91a02c18805c8db5c95e14d1dedba60a
YuyusanK <kurinoyuichi@gmail.com>
2020-08-28 16:47:25 -0700

    db url

--- knexfile.js
+++ knexfile.js
@@ -32,7 +32,7 @@ module.exports = {
 
   staging: {
     client: 'pg',
-    connection: SQLConnection,
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './database/migrations',
       tablename: 'knex_migrations',
